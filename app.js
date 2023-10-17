fcd33f25b0a6209e984ace1e429b2aa55ca72875
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2023-10-17 14:21:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -113,36 +113,36 @@ app
 
                 
                 //test
-                async function main() {
-                    // Generate test SMTP service account from ethereal.email
-                    // Only needed if you don't have a real mail account for testing
-                    let testAccount = await nodemailer.createTestAccount();
+                // async function main() {
+                //     // Generate test SMTP service account from ethereal.email
+                //     // Only needed if you don't have a real mail account for testing
+                //     let testAccount = await nodemailer.createTestAccount();
                   
-                    // create reusable transporter object using the default SMTP transport
-                    let transporter = nodemailer.createTransport({
-                      host: "smtp.ethereal.email",
-                      port: 587,
-                      secure: false, // true for 465, false for other ports
-                      auth: {
-                        user: "tabitha91@ethereal.email", // generated ethereal user
-                        pass: "rDuhMrjmZqXQSPP5es", // generated ethereal password
-                      },
-                    });
+                //     // create reusable transporter object using the default SMTP transport
+                //     let transporter = nodemailer.createTransport({
+                //       host: "smtp.ethereal.email",
+                //       port: 587,
+                //       secure: false, // true for 465, false for other ports
+                //       auth: {
+                //         user: "tabitha91@ethereal.email", // generated ethereal user
+                //         pass: "rDuhMrjmZqXQSPP5es", // generated ethereal password
+                //       },
+                //     });
                     
-                    // send mail with defined transport object
-                    let info = await transporter.sendMail({
-                      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-                      to: "keshawjha400@gmail.com", // list of receivers
-                      subject: "Hello ✔", // Subject line
-                      text: "Hello world?", // plain text body
-                      html: '<img  src="'+src+'"/>img', // html body
-                    });
+                //     // send mail with defined transport object
+                //     let info = await transporter.sendMail({
+                //       from: '"Fred Foo 👻" <foo@example.com>', // sender address
+                //       to: "keshawjha400@gmail.com", // list of receivers
+                //       subject: "Hello ✔", // Subject line
+                //       text: "Hello world?", // plain text body
+                //       html: '<img  src="'+src+'"/>img', // html body
+                //     });
                     
-                    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-                  }
+                //     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+                //   }
                   
-                  main().catch(console.error);
-                //test
+                //   main().catch(console.error);
+                // //test
 
 
 
